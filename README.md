05de85f409544f41bed8afc56abb909896c13e12
Hudson <hudson@ziroau.com>
2026-06-03 12:57:38 +1000

    Split into 2 packs for legacy support

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Fully compatible with other resource packs! As long as this pack is loaded first
 
 ![1](https://raw.githubusercontent.com/hudsonm62/RoundCactusPack/refs/heads/master/screenshots/1.png)
 ![2](https://raw.githubusercontent.com/hudsonm62/RoundCactusPack/refs/heads/master/screenshots/2.png)
+
+Due to changes between 1.12.2 and 1.13+, there are two versions of the pack.
+
+- `Round-Cactus-1.9-1.12.2_VERSION.zip` supports Minecraft 1.9 to 1.12.2
+- `Round-Cactus-main_VERSION.zip` supports Minecraft 1.13 to 26.2.
+
+Ignore any incompatibility warnings, it will work just fine! This is because you couldn't define support for specific versions of Minecraft until 1.20.1.
--- package.ps1
+++ package.ps1
@@ -2,39 +2,46 @@ param(
     [Parameter(Mandatory)]
     [string]$Version,
 
-    [string]$Name = "Round Cactus",
+    [string]$Name = "Round-Cactus",
     [string]$SrcPath = (Join-Path $PSScriptRoot "src"),
     [switch]$FailIfExists = $false
 )
 
-# 1. Copy repo files to src
-(
-    (Join-Path $PSScriptRoot "LICENSE"),
-    (Join-Path $PSScriptRoot "README.md")
-) | ForEach-Object {
-    $DestName = Split-Path $_ -Leaf # file name only
+$legacy = Join-Path $SrcPath "1.9-1.12.2"
+$main = Join-Path $SrcPath "main"
 
-    # remove existing in pack
-    Remove-Item (Join-Path $SrcPath $DestName) -ErrorAction Ignore
-    $dest = Join-Path $SrcPath $DestName
-    Copy-Item $_ -Destination $dest -Force -ErrorAction Stop
-    Write-Information -InformationAction Continue "Copied '$_' to src."
+return @($legacy, $main) | ForEach-Object {
+    $path = $_
+    $pathName = Split-Path $path -Leaf
+    # 1. Copy repo files to src
+    (
+        (Join-Path $PSScriptRoot "LICENSE"),
+        (Join-Path $PSScriptRoot "README.md")
+    ) | ForEach-Object {
+        $DestName = Split-Path $_ -Leaf # file name only
+
+        # remove existing in pack
+        Remove-Item (Join-Path $path $DestName) -ErrorAction Ignore
+        $dest = Join-Path $path $DestName
+        Copy-Item $_ -Destination $dest -Force -ErrorAction Stop
+        Write-Information -InformationAction Continue "Copied '$_' to $pathName."
+    }
+
+    # 2. Zip up src
+    $OutFileName = "${Name}-${pathName}_$Version.zip"
+    $OutputFile = Join-Path $PSScriptRoot $OutFileName
+
+    $Existing = Test-Path $OutputFile
+    if($FailIfExists -and $Existing) {
+        throw "Output file already exists: $OutputFile"
+    }
+    if($Existing) {
+        Write-Information -InformationAction Continue "Output file already exists, deleting: $OutputFile"
+        Remove-Item $OutputFile -Force -ErrorAction Stop
+    }
+
+    # zip up everything with src contents as root
+    try { [System.IO.Compression.ZipFile] | out-null } catch { Add-Type -AssemblyName System.IO.Compression.FileSystem -ErrorAction Stop }
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($path, $OutputFile)
+    Get-Item $OutputFile -Force -ErrorAction Stop
 }
-
-# 2. Zip up src
-$OutFileName = "$Name v$Version.zip"
-$OutputFile = Join-Path $PSScriptRoot $OutFileName
-
-$Existing = Test-Path $OutputFile
-if($FailIfExists -and $Existing) {
-    throw "Output file already exists: $OutputFile"
-}
-if($Existing) {
-    Write-Information -InformationAction Continue "Output file already exists, deleting: $OutputFile"
-    Remove-Item $OutputFile -Force -ErrorAction Stop
-}
-
-# zip up everything with src contents as root
-try { [System.IO.Compression.ZipFile] | out-null } catch { Add-Type -AssemblyName System.IO.Compression.FileSystem -ErrorAction Stop }
-[System.IO.Compression.ZipFile]::CreateFromDirectory($SrcPath, $OutputFile)
-return Get-Item $OutputFile -Force -ErrorAction Stop
